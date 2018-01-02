014e238c9ecb7c58578cdce24998124c5b875b25
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2018-01-02 21:06:27 +0530

    Adding feature of setting of localStorage DB if not found

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,45 @@
 $(document).ready(function () {
 	console.log('document ready');
 
+	var thingsDB = [];
+
 	if (typeof(Storage) !== "undefined") {
 		console.log('localStorage supported!');
-  } else {
-  	alert('Oops! this application will not work, your browser does not support localStorage feature!')
-  }
+
+		thingsDB = localStorage.getItem("lastname");
+		if(thingsDB){
+			thingsDB = JSON.parse(thingsDB);
+			console.log('thingsDB found=>', thingsDB);
+			$('#btnSettings').show();
+		}else{
+			console.log('thingsDB=>', thingsDB);
+			console.log('thingsDB not found setting up DB!');
+			thingsDB = [];
+			// thingsDB.push({
+			// 	name: 'test',
+			// 	on: 'something'
+			// });
+			localStorage.setItem("lastname", JSON.stringify(thingsDB));
+			thingsDB = localStorage.getItem("lastname");
+			console.log('thingsDB=>', thingsDB);
+			console.log('thingsDB=>', JSON.parse(thingsDB));
+		}
+	} else {
+		alert('Oops! this application will not work, your browser does not support localStorage feature!')
+	}
 
 
-  $('#btnAdd').click(function () {
-  	console.log('btnAdd clicked');
+	$('#btnAdd').click(function () {
+		console.log('btnAdd clicked');
 
-  });
+		thingsDB = localStorage.getItem("lastname");
+		console.log('thingsDB=>', thingsDB);
+		if(thingsDB){
+
+		}else{
+			
+		}
+
+	});
 
 });
