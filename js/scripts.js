86c074058c3a5e16386c34c2a28adf16c8c58be9
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2018-01-02 20:17:48 +0530

    Adding localStorage support check

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
 $(document).ready(function () {
-  console.log('document ready');
+	console.log('document ready');
+
+	if (typeof(Storage) !== "undefined") {
+		console.log('localStorage supported!');
+  } else {
+  	alert('Oops! this application will not work, your browser does not support localStorage feature!')
+  }
+
 
   $('#btnAdd').click(function () {
   	console.log('btnAdd clicked');
-  	
+
   });
+
 });
