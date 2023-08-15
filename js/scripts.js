f89b739a1fdba62a862b6054976db852da965b04
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2023-08-15 19:59:23 +0530

    Bumped jQuery to 3.x

--- js/scripts.js
+++ js/scripts.js
@@ -1,121 +1,129 @@
-$(document).ready(function () {
-	console.log('DOM ready');
+$(function () {
+  console.log("DOM ready");
 
-	var thingsDB = [];
+  var thingsDB = [];
 
-	$('.modal').modal();
+  $(".modal").modal();
 
-	$('.datepicker').pickadate({
-		selectMonths: true, // Creates a drop-down to control month
-		selectYears: 50, // Creates a drop-down of 15 years to control year,
-		today: 'Today',
-		clear: 'Clear',
-		close: 'Ok',
-		closeOnSelect: false // Close upon selecting a date,
-	});
+  $(".datepicker").pickadate({
+    selectMonths: true, // Creates a drop-down to control month
+    selectYears: 50, // Creates a drop-down of 15 years to control year,
+    today: "Today",
+    clear: "Clear",
+    close: "Ok",
+    closeOnSelect: false, // Close upon selecting a date,
+  });
 
+  //check for the localStorage support in the browser or not
+  if (typeof Storage !== "undefined") {
+    console.log("localStorage supported!");
+    thingsDB = localStorage.getItem("thingsDB");
+    if (thingsDB) {
+      thingsDB = JSON.parse(thingsDB);
+      console.log("thingsDB found=>", thingsDB);
+      window.document.title = "Things (" + thingsDB.length + ")";
+      $("#btnSettings").show();
 
-	//check for the localStorage support in the browser or not
-	if (typeof(Storage) !== "undefined") {
-		console.log('localStorage supported!');
-		thingsDB = localStorage.getItem("thingsDB");
-		if(thingsDB){
-			thingsDB = JSON.parse(thingsDB);
-			console.log('thingsDB found=>', thingsDB);
-			window.document.title = "Things (" + thingsDB.length +")";
-			$('#btnSettings').show();
+      generateThingsUI();
+    } else {
+      console.log("thingsDB=>", thingsDB);
+      console.log("thingsDB not found setting up DB!");
+      thingsDB = [];
+      localStorage.setItem("thingsDB", JSON.stringify(thingsDB));
+      thingsDB = localStorage.getItem("thingsDB");
+      console.log("thingsDB=>", thingsDB);
+      console.log("thingsDB=>", JSON.parse(thingsDB));
+    }
+  } else {
+    $(".notSupported").slideDown("slow");
+    alert(
+      "Oops! this application will not work, your browser does not support localStorage feature!"
+    );
+  }
 
-			generateThingsUI();
-		}else{
-			console.log('thingsDB=>', thingsDB);
-			console.log('thingsDB not found setting up DB!');
-			thingsDB = [];
-			localStorage.setItem("thingsDB", JSON.stringify(thingsDB));
-			thingsDB = localStorage.getItem("thingsDB");
-			console.log('thingsDB=>', thingsDB);
-			console.log('thingsDB=>', JSON.parse(thingsDB));
-		}
-	} else {
-		$('.notSupported').slideDown('slow');
-		alert('Oops! this application will not work, your browser does not support localStorage feature!')
-	}
+  $("#btnAdd").click(function () {
+    console.log("btnAdd clicked");
+    var txtThingName = $("#txtThingName").val();
+    var txtThingBoughtDate = $("#txtThingBoughtDate").val();
+    console.log("txtThingName=>", txtThingName);
+    console.log("txtThingBoughtDate=>", txtThingBoughtDate);
 
-	$('#btnAdd').click(function () {
-		console.log('btnAdd clicked');
-		var txtThingName = $('#txtThingName').val();
-		var txtThingBoughtDate = $('#txtThingBoughtDate').val();
-		console.log('txtThingName=>', txtThingName);
-		console.log('txtThingBoughtDate=>', txtThingBoughtDate);
-
-		$('.container').append('<div class="row">\
+    $(".container").append(
+      '<div class="row">\
 		<div class="col s12 m4 l2"></div>\
 		<div class="col s12 m4 l8">\
 		<div class="card blue-grey darken-1">\
 		<div class="card-content white-text">\
 		<div class="row">\
 		<div class="input-field col s8">\
-		<span class="card-title">'+txtThingName+'</span>\
+		<span class="card-title">' +
+        txtThingName +
+        '</span>\
 		</div>\
 		<div class="input-field col s4">\
-		<p>'+txtThingBoughtDate+'</p>\
+		<p>' +
+        txtThingBoughtDate +
+        '</p>\
 		</div>\
 		</div>\
 		</div>\
 		</div>\
 		</div>\
 		<div class="col s12 m4 l2"></div>\
-		</div>');
+		</div>'
+    );
 
-		thingsDB = localStorage.getItem("thingsDB");
-		console.log('thingsDB=>', thingsDB);
-		if(thingsDB){
-			thingsDB = JSON.parse(localStorage.getItem("thingsDB"));
-			console.log('thingsDB=>', thingsDB);
-			thingsDB.push({
-				name: txtThingName,
-				boughtDate: txtThingBoughtDate
-			});
-			console.log('after adding thingsDB=>', thingsDB);
-			localStorage.setItem("thingsDB", JSON.stringify(thingsDB));
-		}else{
+    thingsDB = localStorage.getItem("thingsDB");
+    console.log("thingsDB=>", thingsDB);
+    if (thingsDB) {
+      thingsDB = JSON.parse(localStorage.getItem("thingsDB"));
+      console.log("thingsDB=>", thingsDB);
+      thingsDB.push({
+        name: txtThingName,
+        boughtDate: txtThingBoughtDate,
+      });
+      console.log("after adding thingsDB=>", thingsDB);
+      localStorage.setItem("thingsDB", JSON.stringify(thingsDB));
+    } else {
+    }
+  });
 
-		}
+  function generateThingsUI() {
+    console.log("In generateThingsUI");
+    var thingName, thingBoughtDate;
 
-	});
+    thingsDB = JSON.parse(localStorage.getItem("thingsDB"));
 
-	function generateThingsUI() {
-		console.log('In generateThingsUI');
-		var thingName, thingBoughtDate;
+    for (var i = 0; i < thingsDB.length; i++) {
+      thingName = thingsDB[i].name;
+      thingBoughtDate = thingsDB[i].boughtDate;
+      console.log("thingName=>", thingName);
+      console.log("thingBoughtDate=>", thingBoughtDate);
 
-		thingsDB = JSON.parse(localStorage.getItem("thingsDB"));
-
-		for (var i = 0; i < thingsDB.length; i++) {
-			thingName = thingsDB[i].name;
-			thingBoughtDate = thingsDB[i].boughtDate;
-			console.log('thingName=>', thingName);
-			console.log('thingBoughtDate=>', thingBoughtDate);
-
-			$('.container').append('<div class="row">\
+      $(".container").append(
+        '<div class="row">\
 			<div class="col s12 m4 l2"></div>\
 			<div class="col s12 m4 l8">\
 			<div class="card blue-grey darken-1">\
 			<div class="card-content white-text">\
 			<div class="row">\
 			<div class="input-field col s8">\
-			<span class="card-title">'+thingName+'</span>\
+			<span class="card-title">' +
+          thingName +
+          '</span>\
 			</div>\
 			<div class="input-field col s4">\
-			<p>'+thingBoughtDate+'</p>\
+			<p>' +
+          thingBoughtDate +
+          '</p>\
 			</div>\
 			</div>\
 			</div>\
 			</div>\
 			</div>\
 			<div class="col s12 m4 l2"></div>\
-			</div>');
-		}
-
-	}
-
-
+			</div>'
+      );
+    }
+  }
 });
